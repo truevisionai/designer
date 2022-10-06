2b1dac237c86fa5946b3ebb6f7652ea13f011d6a
Himanshu Pal <pal.himanshu1991@gmail.com>
2022-10-06 23:05:54 +0530

    added electron remote

---
+++ preload.js
@@ -0,0 +1,38 @@
+console.log( 'inside preload.js' )
+
+const { contextBridge } = require( 'electron' )
+const remote = require( '@electron/remote' )
+const fs = require('fs')
+
+// NOTE: fs.stat on macos is not working properly this is bug fix
+const statHelper = {
+	isFile: (path) => fs.statSync(path).isFile(),
+	isDirectory: (path) => fs.statSync(path).isDirectory(),
+	isBlockDevice: (path) => fs.statSync(path).isBlockDevice(),
+	isCharacterDevice: (path) => fs.statSync(path).isCharacterDevice(),
+	isSymbolicLink: (path) => fs.statSync(path).isSymbolicLink(),
+	isFIFO: (path) => fs.statSync(path).isFIFO(),
+	isSocket: (path) => fs.statSync(path).isSocket(),
+}
+
+contextBridge.exposeInMainWorld( 'versions', {
+	remote: () => remote,
+	fs: () => fs,
+	fsPromises: () => require('fs/promises'),
+	stat: statHelper
+} )
+
+contextBridge.exposeInMainWorld( 'stat', statHelper )
+
+contextBridge.exposeInMainWorld( 'process', remote.process )
+
+contextBridge.exposeInMainWorld( 'require', remote.require )
+
+contextBridge.exposeInMainWorld( 'fxp', {
+	XMLParser: () => require('fast-xml-parser').XMLParser,
+	XMLBuilder: () => require('fast-xml-parser').XMLBuilder,
+	XMLValidator: () => require('fast-xml-parser').XMLValidator,
+} )
+
+require('fast-xml-parser').XMLParser
+
