20a8f11655cfbb289758422ede14ae720c57c5ab
Himanshu Pal <pal.himanshu1991@gmail.com>
2023-03-28 17:19:04 +0530

    export fixes

--- src/app/services/exporter.service.ts
+++ src/app/services/exporter.service.ts
@@ -4,12 +4,14 @@
 
 import { Injectable } from '@angular/core';
 import { SetToolCommand } from 'app/core/commands/set-tool-command';
+import { GameObject } from 'app/core/game-object';
 import { IFile } from 'app/core/models/file';
 
 import { TvCarlaExporter } from 'app/modules/tv-map/services/tv-carla-exporter';
 import { TvMapInstance } from 'app/modules/tv-map/services/tv-map-source-file';
 import { TvMapService } from 'app/modules/tv-map/services/tv-map.service';
 import { saveAs } from 'file-saver';
+import { Object3D } from 'three';
 
 import { GLTFExporter } from 'three/examples/jsm/exporters/GLTFExporter';
 
@@ -18,6 +20,15 @@ import { FileService } from './file.service';
 import { SceneExporterService } from './scene-exporter.service';
 import { SnackBar } from './snack-bar.service';
 import { TvElectronService } from './tv-electron.service';
+
+import { cloneDeep } from 'lodash';
+
+enum CoordinateSystem {
+	THREE_JS,
+	OPEN_DRIVE,
+	BLENDER,
+	UNITY_GLTF
+}
 
 @Injectable( {
 	providedIn: 'root'
@@ -54,19 +65,69 @@ export class ExporterService {
 
 		const exporter = new GLTFExporter();
 
-		exporter.parse( TvMapInstance.map.gameObject, ( buffer: any ) => {
+		const gameObjectToExport = cloneDeep( TvMapInstance.map.gameObject );
+
+		// Change the coordinate system of the cloned gameObject
+		this.changeCoordinateSystem( gameObjectToExport, CoordinateSystem.OPEN_DRIVE, CoordinateSystem.UNITY_GLTF );
+
+		exporter.parse( gameObjectToExport, ( buffer: any ) => {
 
 			const blob = new Blob( [ buffer ], { type: 'application/octet-stream' } );
 
 			saveAs( blob, filename );
 
-			// forceIndices: true, forcePowerOfTwoTextures: true
-			// to allow compatibility with facebook
 		}, ( error ) => {
 
 		}, { binary: true, forceIndices: true } );
 
 	}
+
+
+
+	changeCoordinateSystem ( gameObject: Object3D, from: CoordinateSystem, to: CoordinateSystem ) {
+
+		// FBX: Y-up (depends on the tool that exported it, but most commonly Y-up)
+		// Three.js: Y-up
+		// OpenDRIVE: Z-up
+		// Blender: Z-up
+		// glTF: Y-up
+		// GLB (Binary glTF): Y-up
+		// OBJ: No specific coordinate system is defined, but typically
+		// it's Y-up or Z-up. The source tool's coordinate system often
+		// determines the exported OBJ file's coordinate system.
+		// Unity3D: Y-up
+		// Unreal Engine: Z-up
+
+		const fromTo = [ from, to ].join( '_' );
+
+		switch ( fromTo ) {
+			case [ CoordinateSystem.THREE_JS, CoordinateSystem.OPEN_DRIVE ].join( '_' ):
+				// THREE.js (Y-up) to OpenDRIVE (Z-up)
+				gameObject.rotateX( Math.PI / 2 );
+				break;
+			case [ CoordinateSystem.OPEN_DRIVE, CoordinateSystem.BLENDER ].join( '_' ):
+				// OpenDRIVE (Z-up) to Blender (Z-up)
+				// No change required
+				break;
+			case [ CoordinateSystem.OPEN_DRIVE, CoordinateSystem.UNITY_GLTF ].join( '_' ):
+				// OpenDRIVE (Z-up) to Unity/glTF (Y-up)
+				gameObject.rotateX( -Math.PI / 2 );
+				break;
+			case [ CoordinateSystem.THREE_JS, CoordinateSystem.BLENDER ].join( '_' ):
+				// THREE.js (Y-up) to Blender (Z-up)
+				gameObject.rotateX( Math.PI / 2 );
+				break;
+			case [ CoordinateSystem.THREE_JS, CoordinateSystem.UNITY_GLTF ].join( '_' ):
+				// THREE.js (Y-up) to Unity/glTF (Y-up)
+				// No change required
+				break;
+			default:
+				console.warn( `Unsupported coordinate system conversion: ${ from } to ${ to }` );
+		}
+
+		gameObject.updateMatrix();
+	}
+
 
 	exportGTLF () {
 
