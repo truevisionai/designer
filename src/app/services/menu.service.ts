076fcffbe5b9bfbbcf2c72899fa19d8722a36bc5
Himanshu Pal <pal.himanshu1991@gmail.com>
2023-03-28 10:37:33 +0530

    basic menu fixes

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -12,63 +12,26 @@ export enum ContextMenuType {
 	HIERARCHY = 3,
 }
 
+declare const menus;
+
 @Injectable( {
 	providedIn: 'root'
 } )
 export class MenuService {
 
-	private menus: Map<number, Menu> = new Map<number, Menu>();
-
-	constructor ( private electron: TvElectronService ) {
-
-	}
+	constructor ( private electron: TvElectronService ) { }
 
 	registerContextMenu ( type: ContextMenuType, template: Array<( MenuItemConstructorOptions ) | ( MenuItem )> ) {
 
 		if ( !this.electron.isElectronApp ) return;
 
-		// TODO: fix
-		const menu = this.electron.remote.Menu.buildFromTemplate( template );
-
-		this.menus.set( type, menu );
+		menus.append( type, template );
 	}
 
 	showContextMenu ( type: ContextMenuType ) {
 
 		if ( !this.electron.isElectronApp ) return;
 
-		if ( this.menus.has( type ) ) {
-
-			// TOOD: fix
-			const menu = this.menus.get( type );
-
-			menu.popup( {
-				window: this.electron.remote.getCurrentWindow()
-			} );
-		}
-	}
-
-	private sampleRegister () {
-
-		// this.menu = electron.remote.require( 'menu' );
-
-		// const menu = electron.remote.Menu.buildFromTemplate( [ {
-		//     label: 'File',
-		//     click: () => {
-		//         console.log( 'clicked' );
-		//     }
-		// } ] );
-
-		// const menu = new Menu();
-		//
-		// menu.append( new MenuItem( {
-		//     label: 'File',
-		//     click: () => {
-		//         console.log( 'clicked' );
-		//     }
-		// } ) );
-
-		// this.registerContextMenu( ContextMenuType.VIEWPORT, menu );
-
+		menus.popup( type );
 	}
 }
