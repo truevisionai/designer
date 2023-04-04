10d458b1c2368187441ab83402067f49f51b6d03
Himanshu Pal <pal.himanshu1991@gmail.com>
2023-04-04 23:40:43 +0530

    refactoring

--- src/app/modules/tv-map/models/prop-polygons.ts
+++ src/app/modules/tv-map/models/prop-polygons.ts
@@ -55,44 +55,210 @@ export class PropPolygon {
 		return mesh;
 	}
 
-	update () {
+	update (): void {
 
 		this.spline.update();
 
+		this.updateMeshGeometry();
+	}
+
+	// Function to update the mesh geometry
+	private updateMeshGeometry (): void {
+
 		if ( this.spline.controlPoints.length < 3 ) return;
 
-		const points: Vector2[] = this.spline.curve.getPoints( 50 ).map(
-			p => new Vector2( p.x, p.y )
-		);
+		const points: Vector2[] = this.generatePoints();
 
-		const shape = new Shape();
+		const shape: Shape = this.createShape( points );
 
-		const first = points.shift();
-
-		shape.moveTo( first.x, first.y );
-
-		shape.splineThru( points );
-
-		this.mesh.geometry.dispose();
-
-		const geometry = this.mesh.geometry = new ShapeGeometry( shape );
-
-		geometry.computeBoundingBox();
+		this.updateGeometry( shape );
 
 		this.updateProps();
 	}
 
+	// Function to generate points from the spline curve
+	private generatePoints (): Vector2[] {
+
+		return this.spline.curve.getPoints( 50 ).map(
+			p => new Vector2( p.x, p.y )
+		);
+
+	}
+
+	// Function to create a shape from the points
+	private createShape ( points: Vector2[] ): Shape {
+
+		const shape = new Shape();
+
+		const first: Vector2 | undefined = points.shift();
+
+		if ( first ) {
+
+			shape.moveTo( first.x, first.y );
+
+			shape.splineThru( points );
+		}
+
+		return shape;
+	}
+
+	// Function to update the geometry
+	private updateGeometry ( shape: Shape ): void {
+
+		this.mesh.geometry.dispose();
+
+		const geometry: ShapeGeometry = this.mesh.geometry = new ShapeGeometry( shape );
+
+		geometry.computeBoundingBox();
+	}
+
+
 	updateProps () {
 
+		this.removeAndClearProps();
+
+		const propInstance = this.getPropInstance();
+
+		const childMeshesAndMaterials = this.getChildMeshesAndMaterials( propInstance );
+
+		const faces = this.computeFaces();
+
+		this.createInstancedMeshes( faces, childMeshesAndMaterials );
+	}
+
+	createInstancedMeshes ( faces: number[][], childMeshesAndMaterials: any ): void {
+
+		const instancedMeshes = this.createInstancedMeshArray( childMeshesAndMaterials );
+
+		let instanceCounter = 0;
+
+		faces.forEach( face => {
+
+			const count = this.calculateInstanceCount( face );
+
+			this.addInstancesToMeshes( count, face, instancedMeshes, instanceCounter );
+
+			instanceCounter += count;
+
+		} );
+
+		this.addInstancedMeshesToScene( instancedMeshes );
+	}
+
+	addInstancedMeshesToScene ( instancedMeshes: InstancedMesh[] ) {
+
+		instancedMeshes.forEach( instancedMesh => {
+
+			SceneService.add( instancedMesh );
+
+			// TODO: fix exporting for this
+			this.props.push( instancedMesh );
+
+		} );
+	}
+
+	addInstancesToMeshes ( count: number, face: number[], instancedMeshes: InstancedMesh[], instanceCounter: number ): void {
+
+		function randomInTriangle ( v1, v2, v3 ) {
+
+			const r1 = Math.random();
+
+			const r2 = Math.sqrt( Math.random() );
+
+			const a = 1 - r2;
+
+			const b = r2 * ( 1 - r1 );
+
+			const c = r1 * r2;
+
+			return ( v1.clone().multiplyScalar( a ) ).add( v2.clone().multiplyScalar( b ) ).add( v3.clone().multiplyScalar( c ) );
+		}
+
+		for ( let i = 0; i < count; i++ ) {
+
+			const position = randomInTriangle(
+				this.spline.controlPointPositions[ face[ 0 ] ],
+				this.spline.controlPointPositions[ face[ 1 ] ],
+				this.spline.controlPointPositions[ face[ 2 ] ]
+			);
+
+			instancedMeshes.forEach( instancedMesh => {
+
+				const instanceMatrix = new Matrix4();
+
+				instanceMatrix.setPosition( position );
+
+				instanceMatrix.multiply( instancedMesh.userData.worldMatrix );
+
+				instancedMesh.setMatrixAt( instanceCounter, instanceMatrix );
+
+				instancedMesh.instanceMatrix.needsUpdate = true;
+
+			} );
+
+			instanceCounter++;
+		}
+	}
+
+	private calculateInstanceCount ( face: number[] ): number {
+
+		const v0 = this.spline.controlPointPositions[ face[ 0 ] ];
+		const v1 = this.spline.controlPointPositions[ face[ 1 ] ];
+		const v2 = this.spline.controlPointPositions[ face[ 2 ] ];
+
+		const t = new Triangle( v0, v1, v2 );
+		const area = t.getArea();
+
+		let count = area * this.density * this.density * this.density * 0.5;
+
+		return Maths.clamp( count, 0, 100 );
+	}
+
+
+	private createInstancedMeshArray ( childMeshesAndMaterials: any ): InstancedMesh[] {
+
+		return childMeshesAndMaterials.map( ( { mesh, material, worldMatrix } ) => {
+
+			const instanceGeometry = mesh.geometry;
+
+			const instanceMaterial = material;
+
+			const maxInstances = 1000; // Compute maxInstances based on your needs
+
+			const instancedMesh = new InstancedMesh( instanceGeometry, instanceMaterial, maxInstances );
+
+			instancedMesh.userData.worldMatrix = worldMatrix;
+
+			return instancedMesh;
+
+		} );
+
+	}
+
+	private removeAndClearProps (): void {
+
+		// Remove props from the scene
 		this.props.forEach( p => SceneService.remove( p ) );
 
+		// Clear the props array
 		this.props.splice( 0, this.props.length );
 
-		const propInstance = AssetDatabase.getInstance( this.propGuid ) as Object3D;
+	}
+
+	private getPropInstance (): Object3D {
+
+		const propInstance: Object3D = AssetDatabase.getInstance( this.propGuid ) as Object3D;
 
 		propInstance.up.set( 0, 0, 1 );
 
 		propInstance.updateMatrixWorld( true );
+
+		return propInstance;
+
+	}
+
+
+	private getChildMeshesAndMaterials ( propInstance: Object3D ): any {
 
 		let childMeshesAndMaterials;
 
@@ -105,6 +271,11 @@ export class PropPolygon {
 			childMeshesAndMaterials = [ { mesh: propInstance, material: propInstance.material } ];
 
 		}
+
+		return childMeshesAndMaterials;
+	}
+
+	private computeFaces (): number[][] {
 
 		const vertices = [];
 
@@ -124,90 +295,7 @@ export class PropPolygon {
 
 		}
 
-		function randomInTriangle ( v1, v2, v3 ) {
-
-			const r1 = Math.random();
-
-			const r2 = Math.sqrt( Math.random() );
-
-			const a = 1 - r2;
-
-			const b = r2 * ( 1 - r1 );
-
-			const c = r1 * r2;
-
-			return ( v1.clone().multiplyScalar( a ) ).add( v2.clone().multiplyScalar( b ) ).add( v3.clone().multiplyScalar( c ) );
-		}
-
-		const instancedMeshes = childMeshesAndMaterials.map( ( { mesh, material, worldMatrix } ) => {
-
-			const instanceGeometry = mesh.geometry;
-
-			const instanceMaterial = material;
-
-			const maxInstances = 100; // Compute maxInstances based on your needs
-
-			const instancedMesh = new InstancedMesh( instanceGeometry, instanceMaterial, maxInstances );
-
-			instancedMesh.userData.worldMatrix = worldMatrix;
-
-			return instancedMesh;
-
-		} );
-
-		let instanceCounter = 0;
-
-		const rotationMatrix = new Matrix4();
-
-		rotationMatrix.makeRotationX( Math.PI / 2 );
-
-		faces.forEach( face => {
-
-			const v0 = this.spline.controlPointPositions[ face[ 0 ] ];
-			const v1 = this.spline.controlPointPositions[ face[ 1 ] ];
-			const v2 = this.spline.controlPointPositions[ face[ 2 ] ];
-
-			const t = new Triangle( v0, v1, v2 );
-
-			const area = t.getArea();
-
-			let count = area * this.density * this.density * this.density * 0.5;
-
-			count = Maths.clamp( count, 0, 100 );
-
-			for ( let i = 0; i < count; i++ ) {
-
-				const position = randomInTriangle( v0, v1, v2 );
-
-				instancedMeshes.forEach( ( instancedMesh ) => {
-
-					const instanceMatrix = new Matrix4();
-
-					instanceMatrix.setPosition( position );
-
-					instanceMatrix.multiply( instancedMesh.userData.worldMatrix );
-
-					instancedMesh.setMatrixAt( instanceCounter, instanceMatrix );
-
-					instancedMesh.instanceMatrix.needsUpdate = true;
-
-				} );
-
-				instanceCounter++;
-			}
-
-		} );
-
-		// Add the instancedMeshes to the scene
-		instancedMeshes.forEach( instancedMesh => {
-
-			SceneService.add( instancedMesh );
-
-			// TODO: fix exporting for this
-			this.props.push( instancedMesh );
-
-		} );
-
+		return faces;
 	}
 
 	private extractMeshesAndMaterials ( group: Group ) {
