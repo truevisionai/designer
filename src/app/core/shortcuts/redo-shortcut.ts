bc7f5be22e6f304326d90fb019672a4e04724ce8
Himanshu Pal <pal.himanshu1991@gmail.com>
2023-04-27 19:08:16 +0530

    redo shortcut fix

--- src/app/core/shortcuts/redo-shortcut.ts
+++ src/app/core/shortcuts/redo-shortcut.ts
@@ -9,9 +9,19 @@ export class RedoShortcut extends BaseShortcut {
 
 	check ( e: KeyboardEvent ): boolean {
 
-		return e.ctrlKey && e.shiftKey && e.key === 'z';
+		const isMac = process.platform === 'darwin';
 
+		if ( isMac ) {
+			// Use Cmd key for macOS
+			return e.metaKey && e.shiftKey && e.key === 'z'
+				|| e.metaKey && e.key === 'y';
+		} else {
+			// Use Ctrl key for Windows/Ubuntu
+			return e.ctrlKey && e.shiftKey && e.key === 'z'
+				|| e.ctrlKey && e.key === 'y';
+		}
 	}
+
 
 	execute (): void {
 
