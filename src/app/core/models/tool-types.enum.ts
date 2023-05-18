5e009350d61ee5e54a58e15b6676cdf137346b84
pal.himanshu1991 <pal.himanshu1991@gmail.com>
2023-05-18 22:30:56 +0530

    tool factory

---
+++ src/app/core/models/tool-types.enum.ts
@@ -0,0 +1,19 @@
+export enum ToolType {
+	Road,
+	RoadCircle,
+	Maneuver,
+	LaneWidth,
+	LaneOffset,
+	PropPoint,
+	PropCurve,
+	PropPolygon,
+	Surface,
+	LaneMarking,
+	LaneAdd,
+	Lane,
+	MarkingPoint,
+	MarkingLine,
+	ParkingBox,
+	Pointer,
+	//... Add more tools as needed
+}
