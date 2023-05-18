7f0e613924ff2add3fed481fd04378ef276d2980
pal.himanshu1991 <pal.himanshu1991@gmail.com>
2023-05-18 22:06:43 +0530

    pointer tool

---
+++ src/app/core/tools/pointer-tool.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright Truesense AI Solutions Pvt Ltd, All Rights Reserved.
+ */
+
+import { BaseTool } from './base-tool';
+
+export class PointerTool extends BaseTool {
+
+	public name: string = 'PointerTool';
+
+	constructor () {
+
+		super();
+
+	}
+
+}
