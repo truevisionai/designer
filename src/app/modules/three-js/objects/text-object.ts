db3d4903e1e6a3d7ed7070675f522cee85d15c79
pal.himanshu1991 <pal.himanshu1991@gmail.com>
2023-05-27 19:40:50 +0530

    radius display support for circle tool

---
+++ src/app/modules/three-js/objects/text-object.ts
@@ -0,0 +1,78 @@
+import { SceneService } from 'app/core/services/scene.service';
+import * as THREE from 'three';
+import { Vector3, Scene } from 'three';
+import { Font, FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+
+export class TextObject {
+
+	private static font: Font;
+	private textMesh: THREE.Mesh;
+
+	private matLite = new THREE.MeshBasicMaterial( {
+		color: this.color,
+		side: THREE.FrontSide
+	} );
+
+	constructor ( private message: string, private position: Vector3, private size: number = 10, private color: number = 0xffffff ) {
+		if ( !TextObject.font ) {
+			const loader = new FontLoader();
+			loader.load( 'assets/fonts/helvetiker_regular.typeface.json', ( font ) => {
+				TextObject.font = font;
+				this.createText();
+			} );
+		} else {
+			this.createText();
+		}
+	}
+
+	private createText () {
+
+		const shapes = TextObject.font.generateShapes( this.message, this.size );
+		const geometry = new THREE.ShapeGeometry( shapes );
+		geometry.computeBoundingBox();
+
+		const xMid = -0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
+		geometry.translate( xMid, 0, 0 );
+
+		this.textMesh = new THREE.Mesh( geometry, this.matLite );
+		this.textMesh.position.copy( this.position );
+
+		SceneService.addHelper( this.textMesh );
+	}
+
+	update ( message: string, position: Vector3, size: number = this.size, color: number = this.color ) {
+		// Remove the old text from the scene
+		SceneService.removeHelper( this.textMesh );
+
+		// Update the text properties
+		this.message = message;
+		this.position = position;
+		this.size = size;
+		this.color = color;
+
+		// Create the new text
+		this.createText();
+	}
+
+	updateText ( message: string ) {
+		// Remove the old text from the scene
+		SceneService.removeHelper( this.textMesh );
+
+		this.message = message;
+
+		this.createText();
+	}
+
+	hide () {
+		this.textMesh.visible = false;
+	}
+
+	show () {
+		this.textMesh.visible = true;
+	}
+
+	remove () {
+		// Remove the text from the scene
+		SceneService.removeHelper( this.textMesh );
+	}
+}
