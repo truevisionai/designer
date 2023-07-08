f998ad07de39c21810c6ee2add5869b2d36dd358
Himanshu Pal <pal.himanshu1991@gmail.com>
2023-07-08 16:57:13 +0530

    progress on box selection

---
+++ src/app/core/factories/junction.factory.ts
@@ -0,0 +1,164 @@
+import { JunctionEntryObject } from 'app/modules/three-js/objects/junction-entry.object';
+import { TvContactPoint, TvLaneType } from 'app/modules/tv-map/models/tv-common';
+import { TvLane } from 'app/modules/tv-map/models/tv-lane';
+import { TvRoad } from 'app/modules/tv-map/models/tv-road.model';
+import { TvMapQueries } from 'app/modules/tv-map/queries/tv-map-queries';
+import { TvMapInstance } from 'app/modules/tv-map/services/tv-map-source-file';
+import { SceneService } from '../services/scene.service';
+
+export class JunctionFactory {
+
+	static createJunctions () {
+
+		const roads = TvMapInstance.map.getRoads();
+
+		const entries = this.getEntries( roads );
+
+		this.findEntryExitCombinations( entries );
+	}
+
+	static showJunctionEntries () {
+
+		const roads = TvMapInstance.map.getRoads();
+
+		const entries = this.getEntries( roads );
+
+		entries.forEach( entry => {
+
+			SceneService.add( entry );
+
+		} );
+	}
+
+	static getAllJunctionEntries (): JunctionEntryObject[] {
+
+		const roads = TvMapInstance.map.getRoads().filter( road => !road.isJunction );
+
+		return this.getEntries( roads );
+
+	}
+
+	static getEntries ( roads: TvRoad[] ) {
+
+		const entries: JunctionEntryObject[] = [];
+
+		for ( let i = 0; i < roads.length; i++ ) {
+
+			entries.push( ...this.createJunctionEntriesForRoad( roads[ i ], TvContactPoint.START ) );
+
+			entries.push( ...this.createJunctionEntriesForRoad( roads[ i ], TvContactPoint.END ) );
+
+		}
+
+		return entries;
+	}
+
+	static autoMergeEntries ( roads: TvRoad[] ) {
+
+		// for ( let i = 0; i < roads.length; i++ ) {
+		//
+		// 	const road = roads[ i ];
+		// 	const start = road.getStartCoord().toVector3();
+		// 	const end = road.getEndCoord().toVector3();
+		//
+		// 	for ( let j = i + 1; j < roads.length; j++ ) {
+		//
+		// 		const otherRoad = roads[ j ];
+		// 		const otherStart = otherRoad.getStartCoord().toVector3();
+		// 		const otherEnd = otherRoad.getEndCoord().toVector3();
+		//
+		// 		if ( start.distanceTo( otherStart ) <= 60 ) {
+		//
+		// 			this.addEntries( road, TvContactPoint.START );
+		// 			this.addEntries( otherRoad, TvContactPoint.START );
+		//
+		// 		}
+		//
+		// 		if ( start.distanceTo( otherEnd ) <= 60 ) {
+		//
+		// 			this.addEntries( road, TvContactPoint.START );
+		// 			this.addEntries( otherRoad, TvContactPoint.END );
+		//
+		// 		}
+		//
+		// 		if ( end.distanceTo( otherEnd ) <= 60 ) {
+		//
+		// 			this.addEntries( road, TvContactPoint.END );
+		// 			this.addEntries( otherRoad, TvContactPoint.END );
+		//
+		// 		}
+		//
+		// 	}
+		//
+		// }
+
+	}
+
+	static findEntryExitCombinations ( objects: JunctionEntryObject[] ) {
+
+		const results = [];
+
+		for ( let i = 0; i < objects.length; i++ ) {
+
+			const left = objects[ i ];
+
+			for ( let j = i + 1; j < objects.length; j++ ) {
+
+				const right = objects[ j ];
+
+				// dont merge same road
+				if ( left.road.id == right.road.id ) continue;
+
+				// we only want to merge
+				// 1 with 1 or
+				// -1 with 1 or
+				// 1 with -1
+				// -1 with -1
+				// to ensure we have straight connections first
+				if ( Math.abs( left.lane.id ) != Math.abs( right.lane.id ) ) continue;
+
+				// dont merge if both are entries
+				if ( left.isEntry && right.isEntry ) continue;
+
+				// dont merge if both are exits
+				if ( left.isExit && right.isExit ) continue;
+
+				const entry = left.isEntry ? left : right;
+
+				const exit = left.isExit ? left : right;
+
+				results.push( {
+					entry: entry,
+					exit: exit
+				} );
+
+			}
+
+		}
+
+		return results;
+
+	}
+
+	static createJunctionEntriesForRoad ( road: TvRoad, contact: TvContactPoint ) {
+
+		const laneSection = contact == TvContactPoint.START ?
+			road.getFirstLaneSection() :
+			road.getLastLaneSection();
+
+		const lanes = laneSection.getLaneArray().filter( lane => lane.id !== 0 && lane.type === TvLaneType.driving );
+
+		return lanes.map( lane => this.createJunctionEntry( road, lane, contact ) );
+	}
+
+	static createJunctionEntry ( road: TvRoad, lane: TvLane, contact: TvContactPoint ): JunctionEntryObject {
+
+		const s = contact == TvContactPoint.START ? 0 : road.length;
+
+		const position = TvMapQueries.getLanePosition( road.id, lane.id, s );
+
+		const name = `road-${ road.id }-lane-${ lane.id }-${ contact }`;
+
+		return new JunctionEntryObject( name, position, contact, road, lane );
+	}
+}
--- src/app/core/tools/maneuver/maneuver-tool.ts
+++ src/app/core/tools/maneuver/maneuver-tool.ts
@@ -7,29 +7,31 @@ import { ISelectable } from 'app/modules/three-js/objects/i-selectable';
 import { JunctionEntryObject } from 'app/modules/three-js/objects/junction-entry.object';
 import { RoadControlPoint } from 'app/modules/three-js/objects/road-control-point';
 import { OdLaneDirectionBuilder } from 'app/modules/tv-map/builders/od-lane-direction-builder';
-import { TvContactPoint, TvElementType, TvLaneSide, TvLaneType } from 'app/modules/tv-map/models/tv-common';
+import { TvLaneSide } from 'app/modules/tv-map/models/tv-common';
 import { LanePathObject } from 'app/modules/tv-map/models/tv-junction-lane-link';
 import { TvRoad } from 'app/modules/tv-map/models/tv-road.model';
-import { TvMapQueries } from 'app/modules/tv-map/queries/tv-map-queries';
 import { CommandHistory } from 'app/services/command-history';
 import { SnackBar } from 'app/services/snack-bar.service';
-import { LaneLinkInspector } from 'app/views/inspectors/lane-link-inspector/lane-link-inspector.component';
-import { MultiCmdsCommand } from '../../commands/multi-cmds-command';
 import { IToolWithPoint, SelectPointCommand } from '../../commands/select-point-command';
-import { SetInspectorCommand } from '../../commands/set-inspector-command';
-import { UpdateRoadPointCommand } from '../../commands/update-road-point-command';
+import { JunctionFactory } from '../../factories/junction.factory';
 import { LanePathFactory } from '../../factories/lane-path-factory.service';
+import { BoxSelectionToolHelper } from '../../helpers/box-selection-tool-helper';
 import { KeyboardInput } from '../../input';
 import { ToolType } from '../../models/tool-types.enum';
 import { SceneService } from '../../services/scene.service';
+import { TvConsole } from '../../utils/console';
 import { BaseTool } from '../base-tool';
 import { CreateJunctionConnection } from './create-junction-connection';
+import { SelectionBox } from 'three/examples/jsm/interactive/SelectionBox';
+import { SelectionHelper } from 'three/examples/jsm/interactive/SelectionHelper';
+import { AppService } from 'app/core/services/app.service';
 
 const DEFAULT_SIDE = TvLaneSide.RIGHT;
 
 export class ManeuverTool extends BaseTool implements IToolWithPoint {
 
 	name: string = 'ManeuverTool';
+
 	toolType = ToolType.Maneuver;
 
 	public connectingRoad: TvRoad;
@@ -47,127 +49,16 @@ export class ManeuverTool extends BaseTool implements IToolWithPoint {
 
 	public junctionEntryObject: JunctionEntryObject;
 
-	setPoint ( value: ISelectable ): void {
-		this.junctionEntryObject = value as JunctionEntryObject;
-	}
+	// private selectionHelper = new BoxSelectionToolHelper();
+	private selectionHelper: SelectionHelper;
+	private selectionBox: SelectionBox;
 
-	getPoint (): ISelectable {
-		return this.junctionEntryObject;
-	}
-
-	finalList = new Map<number, JunctionEntryObject>();
-
-	addEntries ( road: TvRoad, contact: TvContactPoint ) {
-
-		this.junctionEntryObjects
-			.filter( entry => entry.road.id == road.id )
-			.filter( entry => entry.contact == contact )
-			.forEach( entry => {
-				if ( !this.finalList.has( entry.id ) ) {
-					this.finalList.set( entry.id, entry );
-				}
-			} );
-
-	}
 
 	init () {
 
+		// this.selectionHelper = new SelectionHelper( AppService.three.renderer, 'selectBox' );
+		// this.selectionBox = new SelectionBox( AppService.three.camera, SceneService.scene );
 
-		// find all nodes
-		setTimeout( () => {
-
-			// console.log( this.junctionEntryObjects )
-
-			const roads = this.map.getRoads();
-
-			for ( let i = 0; i < roads.length; i++ ) {
-
-				const road = roads[ i ];
-				const start = road.getStartCoord().toVector3();
-				const end = road.getEndCoord().toVector3();
-
-				for ( let j = i + 1; j < roads.length; j++ ) {
-
-					const otherRoad = roads[ j ];
-					const otherStart = otherRoad.getStartCoord().toVector3();
-					const otherEnd = otherRoad.getEndCoord().toVector3();
-
-					if ( start.distanceTo( otherStart ) <= 60 ) {
-
-						this.addEntries( road, TvContactPoint.START );
-						this.addEntries( otherRoad, TvContactPoint.START );
-
-					}
-
-					if ( start.distanceTo( otherEnd ) <= 60 ) {
-
-						this.addEntries( road, TvContactPoint.START );
-						this.addEntries( otherRoad, TvContactPoint.END );
-
-
-					}
-
-					if ( end.distanceTo( otherEnd ) <= 60 ) {
-
-						this.addEntries( road, TvContactPoint.END );
-						this.addEntries( otherRoad, TvContactPoint.END );
-
-					}
-
-				}
-
-			}
-
-			// console.log( this.finalList )
-
-			this.finalList.forEach( entry => {
-				entry.select();
-			} );
-
-			this.mergeEntries( Array.from( this.finalList.values() ) );
-
-		}, 2000 );
-
-	}
-
-	mergeEntries ( entries: JunctionEntryObject[] ) {
-
-		for ( let i = 0; i < entries.length; i++ ) {
-
-			const left = entries[ i ];
-
-			for ( let j = i + 1; j < entries.length; j++ ) {
-
-				const right = entries[ j ];
-
-				// dont merge same road
-				if ( left.road.id == right.road.id ) continue;
-
-				// we only want to merge
-				// 1 with 1 or
-				// -1 with 1 or
-				// 1 with -1
-				// -1 with -1
-				// to ensure we have straight connections first
-				if ( Math.abs( left.lane.id ) != Math.abs( right.lane.id ) ) continue;
-
-				// dont merge if both are entries
-				if ( left.isEntry && right.isEntry ) continue;
-
-				// dont merge if both are exits
-				if ( left.isExit && right.isExit ) continue;
-
-				// console.log( 'connecting', left, right );
-
-				const entry = left.isEntry ? left : right;
-
-				const exit = left.isExit ? left : right;
-
-				this.connectJunctionObject( entry, exit );
-
-			}
-
-		}
 
 	}
 
@@ -180,7 +71,8 @@ export class ManeuverTool extends BaseTool implements IToolWithPoint {
 		this.connectingRoad = null;
 		this.roadControlPoint = null;
 
-		this.showJunctionEntries();
+		this.junctionEntryObjects = JunctionFactory.getAllJunctionEntries();
+		this.junctionEntryObjects.forEach( obj => SceneService.add( obj ) );
 
 		this.showLanePathObjects();
 	}
@@ -189,112 +81,93 @@ export class ManeuverTool extends BaseTool implements IToolWithPoint {
 
 		super.disable();
 
+		// this.selectionHelper.dispose();
+
 		this.junctionEntryObject = null;
 		this.lanePathObject = null;
 		this.connectingRoad = null;
 		this.roadControlPoint = null;
 
-		this.hideJunctionEntries();
+		this.junctionEntryObjects.forEach( obj => SceneService.remove( obj ) );
+		this.junctionEntryObjects.splice( 0, this.junctionEntryObjects.length );
 
 		this.hideLanePathObjects();
 
 		this.laneDirectionHelper.clear();
 	}
 
-	onPointerDown ( e: PointerEventData ) {
+	setPoint ( value: ISelectable ): void {
+		this.junctionEntryObject = value as JunctionEntryObject;
+	}
+
+	getPoint (): ISelectable {
+		return this.junctionEntryObject;
+	}
+
+
+	onPointerClicked ( e: PointerEventData ) {
 
 		if ( e.button != MouseButton.LEFT ) return;
 
 		const shiftKeyDown = KeyboardInput.isShiftKeyDown;
 
-		// if ( !shiftKeyDown && this.checkRoadControlPointInteraction( e ) ) return;
+		// if ( shiftKeyDown ) {
+
+		// 	this.selectionHelper.start( e );
+
+		// }
+
+
+		if ( !shiftKeyDown ) return;
 
 		if ( !shiftKeyDown && this.hasClickedJunctionObject( e ) ) return;
-
-		// if ( !shiftKeyDown && this.checkPathInteraction( e ) ) return;
-
-		// const commands = [];
-		//
-		// commands.push( new SetInspectorCommand( null, null ) );
-		//
-		// CommandHistory.execute( new MultiCmdsCommand( commands ) );
-		//
-		// if ( this.connectingRoad ) {
-		//
-		// 	this.connectingRoad.hideNodes();
-		// 	this.connectingRoad.spline.hide();
-		// }
 
 		CommandHistory.execute( new SelectPointCommand( this, null ) );
 
 		this.setHint( 'Select two junction points to create a new junction connection' );
-
-		// if ( this.lanePathObject ) this.lanePathObject.visible = false;
 	}
 
 	onPointerUp ( e: PointerEventData ) {
 
-		if ( this.connectingRoad && this.roadControlPoint && this.roadChanged ) {
+		// if ( this.selectionHelper.isSelecting ) {
 
-			const updateRoadPointCommand = new UpdateRoadPointCommand(
-				this.connectingRoad,
-				this.roadControlPoint,
-				this.roadControlPoint.position,
-				this.pointerDownAt
-			);
+		// 	this.selectionHelper.end( e )
 
-			CommandHistory.execute( updateRoadPointCommand );
+		// }
 
-			LanePathFactory.update( this.lanePathObject );
-		}
-
-		this.roadChanged = false;
 	}
 
 	onPointerMoved ( e: PointerEventData ) {
 
-		if ( this.isPointerDown && this.roadControlPoint && this.connectingRoad ) {
+		// if ( this.selectionHelper.isSelecting ) {
 
-			this.roadControlPoint.copyPosition( e.point );
+		// 	this.selectionHelper.update( e );
 
-			this.connectingRoad.spline.update();
+		// }
 
-			this.roadChanged = true;
+		// if ( BoxSelectionToolHelper.selectionHelper.isDown ) {
+		//
+		// 	BoxSelectionToolHelper.selectionBox.collection
+		// 		.filter( i => i[ 'tag' ] = JunctionEntryObject.tag )
+		// 		.forEach( ( object: any ) => {
+		// 			if ( object instanceof JunctionEntryObject ) {
+		// 				object.unselect();
+		// 			}
+		// 		} );
+		//
+		// 	BoxSelectionToolHelper.selectionBox.endPoint.set( e.mouse.x, e.mouse.y, 0.5 );
+		//
+		// 	const allSelected = BoxSelectionToolHelper.selectionBox.select();
+		//
+		// 	allSelected
+		// 		.filter( i => i[ 'tag' ] == JunctionEntryObject.tag )
+		// 		.forEach( ( object: any ) => {
+		// 			if ( object instanceof JunctionEntryObject ) {
+		// 				object.select();
+		// 			}
+		// 		} );
+		// }
 
-		}
-
-	}
-
-	checkPathInteraction ( event: PointerEventData ): boolean {
-
-		if ( event.button !== MouseButton.LEFT ) return;
-
-		let hasInteracted = false;
-
-		for ( let i = 0; i < event.intersections.length; i++ ) {
-
-			const intersection = event.intersections[ i ];
-
-			// tslint:disable-next-line: no-string-literal
-			if ( intersection.object[ 'tag' ] === LanePathObject.tag ) {
-
-				hasInteracted = true;
-
-				this.lanePathObject = intersection.object.parent as LanePathObject;
-
-				const commands = [];
-
-				this.connectingRoad = this.lanePathObject.connectingRoad;
-
-				commands.push( new SetInspectorCommand( LaneLinkInspector, this.lanePathObject ) );
-
-				CommandHistory.execute( new MultiCmdsCommand( commands ) );
-
-				break;
-			}
-		}
-
-		return hasInteracted;
 	}
 
 	hasClickedJunctionObject ( event: PointerEventData ): boolean {
@@ -313,11 +186,11 @@ export class ManeuverTool extends BaseTool implements IToolWithPoint {
 
 			this.setHint( 'connect junction objects' );
 
-			const entryExitSide = this.validateEntryExitCombination( this.junctionEntryObject, junctionObject );
+			const entryExitSide = this.isValidEntryExit( this.junctionEntryObject, junctionObject );
 
 			if ( !entryExitSide ) return;
 
-			this.connectJunctionObject( this.junctionEntryObject, junctionObject );
+			this.createJunctionConnection( entryExitSide.entry, entryExitSide.exit );
 
 		} else {
 
@@ -330,7 +203,7 @@ export class ManeuverTool extends BaseTool implements IToolWithPoint {
 		return true;
 	}
 
-	connectJunctionObject ( entry: JunctionEntryObject, exit: JunctionEntryObject ): void {
+	createJunctionConnection ( entry: JunctionEntryObject, exit: JunctionEntryObject ): void {
 
 		try {
 
@@ -368,14 +241,16 @@ export class ManeuverTool extends BaseTool implements IToolWithPoint {
 	}
 
 
-	validateEntryExitCombination ( a: JunctionEntryObject, b: JunctionEntryObject ) {
+	isValidEntryExit ( a: JunctionEntryObject, b: JunctionEntryObject ) {
 
 		// Error Handling: Check if a and b are defined
-		if ( !a || !b ) throw new Error( 'Both a and b must be defined.' );
+		if ( !a || !b ) TvConsole.error( 'Both a and b must be defined.' );
+		if ( !a || !b ) return;
 
 		// Assuming a and b are instances of JunctionEntryObject, they should have the properties 'lane' and 'contact'
 		// If these properties are not defined, throw an error
-		if ( !a.lane || !a.contact || !b.lane || !b.contact ) throw new Error( 'a and b must have the properties \'lane\' and \'contact\'.' );
+		if ( !a.lane || !a.contact || !b.lane || !b.contact ) TvConsole.error( 'a and b must have the properties \'lane\' and \'contact\'.' );
+		if ( !a.lane || !a.contact || !b.lane || !b.contact ) return;
 
 		if ( a.isEntry && b.isEntry || a.isExit && b.isExit ) SnackBar.warn( 'Cannot connect two entries or two exits.' );
 		if ( a.isEntry && b.isEntry || a.isExit && b.isExit ) return;
@@ -385,26 +260,6 @@ export class ManeuverTool extends BaseTool implements IToolWithPoint {
 			exit: b.isExit ? b : a,
 			side: TvLaneSide.RIGHT
 		};
-
-	}
-
-	showJunctionEntries () {
-
-		this.map.getRoads().filter( road => !road.isJunction ).forEach( road => {
-
-			if ( !road.predecessor || road.predecessor.elementType === TvElementType.junction ) {
-
-				this.showStartEntries( road );
-
-			}
-
-			if ( !road.successor || road.successor.elementType === TvElementType.junction ) {
-
-				this.showEndEntries( road );
-
-			}
-
-		} );
 
 	}
 
@@ -429,58 +284,6 @@ export class ManeuverTool extends BaseTool implements IToolWithPoint {
 			} );
 
 		} );
-
-	}
-
-	showStartEntries ( road: TvRoad ) {
-
-		road.getFirstLaneSection()
-			.getLaneArray()
-			.filter( lane => lane.id !== 0 && lane.type === TvLaneType.driving )
-			.forEach( lane => {
-
-				this.laneDirectionHelper.drawSingleLane( road, lane );
-
-				const position = TvMapQueries.getLanePosition( road.id, lane.id, 0 );
-
-				const name = `road-${ road.id }-lane-${ lane.id }-${ TvContactPoint.START }`;
-
-				const obj = new JunctionEntryObject( name, position, TvContactPoint.START, road, lane );
-
-				this.junctionEntryObjects.push( obj );
-
-				SceneService.add( obj );
-
-			} );
-	}
-
-	showEndEntries ( road: TvRoad ) {
-
-		road.getLastLaneSection()
-			.getLaneArray()
-			.filter( lane => lane.id !== 0 && lane.type === TvLaneType.driving )
-			.forEach( lane => {
-
-				this.laneDirectionHelper.drawSingleLane( road, lane );
-
-				const position = TvMapQueries.getLanePosition( road.id, lane.id, road.length );
-
-				const name = `road-${ road.id }-lane-${ lane.id }-${ TvContactPoint.END }`;
-
-				const obj = new JunctionEntryObject( name, position, TvContactPoint.END, road, lane );
-
-				this.junctionEntryObjects.push( obj );
-
-				SceneService.add( obj );
-
-			} );
-	}
-
-	hideJunctionEntries () {
-
-		this.junctionEntryObjects.forEach( obj => SceneService.remove( obj ) );
-
-		this.junctionEntryObjects.splice( 0, this.junctionEntryObjects.length );
 
 	}
 
