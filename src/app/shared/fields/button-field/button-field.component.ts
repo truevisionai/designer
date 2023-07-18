1bc41b69bac1db9d1ca82050f2ab5598bb7c8130
Himanshu Pal <pal.himanshu1991@gmail.com>
2023-07-18 19:25:18 +0530

    button field

---
+++ src/app/shared/fields/button-field/button-field.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractFieldComponent } from 'app/core/components/abstract-field.component';
+
+@Component( {
+	selector: 'app-button-field',
+	templateUrl: './button-field.component.html',
+	styleUrls: [ './button-field.component.scss' ]
+} )
+export class ButtonFieldComponent {
+
+	@Output() clicked = new EventEmitter<void>();
+
+	@Input() value: any;
+
+	@Input() disabled: boolean = false;
+
+	@Input() label: string;
+
+	constructor () {
+
+	}
+
+
+}
