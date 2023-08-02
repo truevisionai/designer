998416c1dabfe54fc6a3ab71dcf7350c96e09064
Himanshu Pal <pal.himanshu1991@gmail.com>
2023-08-02 15:08:26 +0530

    main menu to allow copy paste

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
  */
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, Menu, screen  } = require( 'electron' );
+const { app, BrowserWindow, Menu, screen } = require( 'electron' );
 const path = require( 'path' );
 const log = require( 'electron-log' );
 
@@ -211,4 +211,26 @@ app.on( 'activate', function () {
 //     event.returnValue = __dirname;
 // } );
 
-Menu.setApplicationMenu( null );
+// Create the Application's main menu
+var template = [
+	{
+		label: TITLE,
+		submenu: [
+			{ label: "Quit", accelerator: "Command+Q", click: function () { app.quit(); } }
+		]
+	},
+	{
+		label: "Edit",
+		submenu: [
+			{ label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+			{ label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+			{ type: "separator" },
+			{ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+			{ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+			{ label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+			{ label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+		]
+	}
+];
+
+Menu.setApplicationMenu( Menu.buildFromTemplate( template ) );
