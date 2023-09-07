93b5d7d378aa26328a610646555af58111f425b5
Himanshu Pal <pal.himanshu1991@gmail.com>
2023-09-07 15:44:23 +0530

    change material on color change

--- src/app/modules/tv-map/models/tv-object-marking.ts
+++ src/app/modules/tv-map/models/tv-object-marking.ts
@@ -2,13 +2,13 @@
  * Copyright Truesense AI Solutions Pvt Ltd, All Rights Reserved.
  */
 
+import { AssetDatabase } from 'app/core/asset/asset-database';
 import { MarkingObjectFactory } from 'app/core/factories/marking-object.factory';
 import * as THREE from 'three';
 import { CatmullRomCurve3, Mesh, Vector3 } from 'three';
+import { COLOR } from '../../../shared/utils/colors.service';
 import { TvColors, TvRoadMarkWeights, TvSide } from './tv-common';
 import { TvCornerRoad, TvRoadObject } from './tv-road-object';
-import { AssetDatabase } from 'app/core/asset/asset-database';
-import { RoadFactory } from 'app/core/factories/road-factory.service';
 
 export class TvObjectMarking {
 
@@ -22,18 +22,19 @@ export class TvObjectMarking {
 	 * Specifies a marking that is either attached to one side of the
 	 * object’s bounding box or referencing outline points.
 	 *
+	 * @param _color color of the marking
 	 * @param side Side of the bounding box described in <object> element in the local coordinate system u/v
 	 * @param weight optical "weight" of the marking
-	 * @param color  color of the marking
 	 * @param spaceLength Length of the gap between the visible parts
 	 * @param lineLength length of a line segment between two spaces
 	 * @param startOffset Lateral offset in u-direction from start of bounding box side where the first marking starts
 	 * @param stopOffset Lateral offset in u-direction from end of bounding box side where the marking ends
 	 * @param zOffset Height of road mark above the road, i.e. thickness of the road mark
 	 * @param width width of the marking (attribute is optional if detailed definition is given below)
+	 * @param cornerReferences
 	 */
 	constructor (
-		public color: TvColors = TvColors.WHITE,
+		private _color: TvColors = TvColors.WHITE,
 		public spaceLength: number = 0.3,
 		public lineLength: number = 0.3,
 		public side: TvSide = TvSide.FRONT,
@@ -44,7 +45,7 @@ export class TvObjectMarking {
 		public width: number = 1.83,
 		public cornerReferences: number[] = [] // 2 or more corners,
 	) {
-		this._material = new THREE.MeshBasicMaterial( { color: color } );
+		this._material = new THREE.MeshBasicMaterial( { color: _color } );
 	}
 
 	get material (): THREE.MeshBasicMaterial {
@@ -53,6 +54,16 @@ export class TvObjectMarking {
 
 	set material ( value: THREE.MeshBasicMaterial ) {
 		this._material = value;
+	}
+
+	get color (): TvColors {
+		return this._color;
+	}
+
+	set color ( value: TvColors ) {
+		this._color = value;
+		this._material?.color.set( COLOR.stringToColor( value ) );
+		this._material.needsUpdate = true;
 	}
 
 	get materialGuid (): string {
