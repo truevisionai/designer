61d3e155532e17ad9a3b55f120e4eb31196d25ed
Himanshu Pal <pal.himanshu1991@gmail.com>
2023-10-25 17:28:06 +0530

    snapping to lane and road

--- src/app/core/snapping/move-strategies/lane-end-move.strategy.ts
+++ src/app/core/snapping/move-strategies/lane-end-move.strategy.ts
@@ -7,11 +7,14 @@ import { TvLane } from '../../../modules/tv-map/models/tv-lane';
 import { TvPosTheta } from '../../../modules/tv-map/models/tv-pos-theta';
 import { TvMapQueries } from '../../../modules/tv-map/queries/tv-map-queries';
 import { IHasSCoord } from '../snap-strategies/snapping';
-import { MoveStrategy } from './move-strategy';
+import { MovingStrategy } from './move-strategy';
+import { PointerEventData } from 'app/events/pointer-event-data';
+import { Position } from 'app/modules/scenario/models/position';
 
-export class LaneEndMoveStrategy implements MoveStrategy {
+export class LaneEndMoveStrategy extends MovingStrategy {
 
 	constructor ( private lane: TvLane, private sValues: IHasSCoord[] = [] ) {
+		super();
 	}
 
 	getPosTheta ( position: Vector3 ): TvPosTheta {
@@ -35,4 +38,11 @@ export class LaneEndMoveStrategy implements MoveStrategy {
 		return TvMapQueries.getLaneEndPosition( this.lane.roadId, this.lane.id, s );
 
 	}
+
+	getPosition ( e: PointerEventData ): Position {
+
+		throw new Error( 'Method not implemented.' );
+
+	}
+
 }
--- src/app/core/snapping/move-strategies/move-strategy.ts
+++ src/app/core/snapping/move-strategies/move-strategy.ts
@@ -3,180 +3,208 @@
  */
 
 import { PointerEventData } from 'app/events/pointer-event-data';
-import { RoadControlPoint } from 'app/modules/three-js/objects/road-control-point';
-import { RoadTangentPoint } from 'app/modules/three-js/objects/road-tangent-point';
-import { Raycaster, Vector3 } from 'three';
+import { Vector3 } from 'three';
 import { TvPosTheta } from '../../../modules/tv-map/models/tv-pos-theta';
-import { SelectStrategy } from '../select-strategies/select-strategy';
-import { ThreeService } from 'app/modules/three-js/three.service';
-import { TvMapInstance } from 'app/modules/tv-map/services/tv-map-source-file';
+import { TvLaneCoord, TvRoadCoord } from 'app/modules/tv-map/models/tv-lane-coord';
+import { Position } from 'app/modules/scenario/models/position';
+import { TvMapQueries } from 'app/modules/tv-map/queries/tv-map-queries';
 
-export interface MoveStrategy {
+
+interface IMovingStrategy {
+
 	getPosTheta ( position: Vector3 ): TvPosTheta;
 
 	getVector3 ( s: number ): Vector3;
+
+	getPosition ( e: PointerEventData ): Position;
+
 }
 
-export class RoadPointMovingStrategy implements MoveStrategy {
+export abstract class MovingStrategy implements IMovingStrategy {
 
-	constructor ( private point: RoadControlPoint ) {
+	abstract getPosition ( event: PointerEventData ): Position;
+
+	init (): void {
+		//
+	}
+
+	enable (): void {
+		//
+	}
+
+	disable (): void {
+		//
+	}
+
+	destroy (): void {
+		//
 	}
 
 	getPosTheta ( position: Vector3 ): TvPosTheta {
 
-		const original = new TvPosTheta( position.x, position.y, position.z, this.point.hdg );
+		throw new Error( 'Method not implemented.' );
 
+	}
 
-		if ( this.point.road.isJunction ) {
+	getVector3 ( s: number ): Vector3 {
 
-			// return if point is first or last control point
-			if ( this.point.tagindex === 0 ) return original;
+		throw new Error( 'Method not implemented.' );
 
-			if ( this.point.tagindex === this.point.road.spline.controlPoints.length - 1 ) return original;
+	}
 
-			const previous = this.point.road.spline.controlPoints[ this.point.tagindex - 1 ] as RoadControlPoint;
+	protected onLaneCoord ( event: PointerEventData ): TvLaneCoord {
 
-			if ( !previous ) return original;
+		const roadCoord = TvMapQueries.findRoadCoord( event.point );
 
-			const hdg = previous.hdg;
+		if ( !roadCoord ) return;
 
-			const final = calculatePointC( this.point.position, hdg, position );
+		const laneSection = roadCoord.road.getLaneSectionAt( roadCoord.s );
 
-			return new TvPosTheta( final.x, final.y, final.z, hdg );
+		const t = roadCoord.t;
+
+		const lanes = laneSection.lanes;
+
+		const isLeft = t > 0;
+		const isRight = t < 0;
+
+		for ( const [ id, lane ] of lanes ) {
+
+			// logic to skip left or right lanes depending on t value
+			if ( isLeft && lane.isRight ) continue;
+			if ( isRight && lane.isLeft ) continue;
+
+			const startT = laneSection.getWidthUptoStart( lane, roadCoord.s );
+			const endT = laneSection.getWidthUptoEnd( lane, roadCoord.s );
+
+			if ( Math.abs( t ) > startT && Math.abs( t ) < endT ) {
+
+				return new TvLaneCoord( roadCoord.roadId, laneSection.id, lane.id, roadCoord.s, 0 );
+
+			}
 
 		}
-
-		return original;
-	}
-
-	getVector3 ( s: number ): Vector3 {
-
-		const position = this.point.road.getPositionAt( s );
-
-		return position.toVector3();
-
-	}
-
-
-}
-
-export class RoadTangentMovingStrategy implements MoveStrategy {
-
-	constructor ( private tangent: RoadTangentPoint ) {
-	}
-
-	getPosTheta ( position: Vector3 ): TvPosTheta {
-
-		// our main point
-		const point = this.tangent.controlPoint;
-
-		if ( this.tangent.road.isJunction ) {
-
-			const hdg = point.hdg;
-
-			const final = calculatePointC( this.tangent.position, hdg, position );
-
-			return new TvPosTheta( final.x, final.y, final.z, hdg );
-
-		} else {
-
-			return new TvPosTheta( position.x, position.y, position.z, point.hdg );
-
-		}
-
-
-	}
-
-	getVector3 ( s: number ): Vector3 {
-
-		const position = this.tangent.road.getPositionAt( s );
-
-		return position.toVector3();
-
-	}
-
-
-}
-
-// Function to calculate the position of point C
-function calculatePointC ( A: Vector3, hdg: number, B: Vector3 ): Vector3 {
-	// Step 1: Find the direction vector of A's heading
-	const dir_vector_A = new Vector3( Math.cos( hdg ), Math.sin( hdg ), 0 );
-
-	// Step 2: Find the vector between A and B
-	const AB_vector = B.clone().sub( A );
-
-	// Step 3: Project AB_vector onto the direction of dir_vector_A
-	const projection_scalar = AB_vector.dot( dir_vector_A );
-	const projection_vector = dir_vector_A.clone().multiplyScalar( projection_scalar );
-
-	// Step 4: Calculate the position of point C
-	const C = A.clone().add( projection_vector );
-
-	return C;
-}
-
-export class RoadPointerStrategy extends SelectStrategy<RoadControlPoint> {
-
-	onPointerDown ( pointerEventData: PointerEventData ): RoadControlPoint {
-
-		throw new Error( 'Method not implemented.' );
-
-	}
-
-	onPointerMoved ( pointerEventData: PointerEventData ): RoadControlPoint {
-
-		throw new Error( 'Method not implemented.' );
-
-	}
-
-	onPointerUp ( pointerEventData: PointerEventData ): RoadControlPoint {
-
-		throw new Error( 'Method not implemented.' );
-
-	}
-
-	dispose (): void {
-		throw new Error( 'Method not implemented.' );
 	}
 
 }
 
-export class MovePointStrategy implements MoveStrategy {
+// export class RoadPointMovingStrategy implements MoveStrategy {
 
-	private raycaster = new Raycaster();
+// 	constructor ( private point: RoadControlPoint ) {
+// 	}
 
-	constructor () {
+// 	getPosTheta ( position: Vector3 ): TvPosTheta {
 
-		this.raycaster.far = 1000;
+// 		const original = new TvPosTheta( position.x, position.y, position.z, this.point.hdg );
 
-	}
+// 		if ( this.point.road.isJunction ) {
 
+// 			// return if point is first or last control point
+// 			if ( this.point.tagindex === 0 ) return original;
 
-	getPosTheta ( position: Vector3 ): TvPosTheta {
+// 			if ( this.point.tagindex === this.point.road.spline.controlPoints.length - 1 ) return original;
 
-		throw new Error( 'Method not implemented.' );
+// 			const previous = this.point.road.spline.controlPoints[ this.point.tagindex - 1 ] as RoadControlPoint;
 
-	}
+// 			if ( !previous ) return original;
 
-	getVector3 ( s: number ): Vector3 {
+// 			const hdg = previous.hdg;
 
-		throw new Error( 'Method not implemented.' );
+// 			const final = calculatePointC( this.point.position, hdg, position );
 
-	}
+// 			return new TvPosTheta( final.x, final.y, final.z, hdg );
 
-	getPosition ( e: PointerEventData ): Vector3 {
+// 		}
 
-		this.raycaster.setFromCamera( e.mouse, e.camera );
+// 		return original;
+// 	}
 
-		const raycastableObjects = [ TvMapInstance.map.gameObject, ThreeService.bgForClicks ];
+// 	getVector3 ( s: number ): Vector3 {
 
-		const intersections = this.raycaster.intersectObjects( raycastableObjects, true ).filter( i => i.object.visible );
+// 		const position = this.point.road.getPositionAt( s );
 
-		if ( intersections.length > 0 ) return intersections[ 0 ].point;
+// 		return position.toVector3();
 
-		return e.point;
+// 	}
 
-	}
+// }
 
-}
+// export class RoadTangentMovingStrategy implements MoveStrategy {
+
+// 	constructor ( private tangent: RoadTangentPoint ) {
+// 	}
+
+// 	getPosTheta ( position: Vector3 ): TvPosTheta {
+
+// 		// our main point
+// 		const point = this.tangent.controlPoint;
+
+// 		if ( this.tangent.road.isJunction ) {
+
+// 			const hdg = point.hdg;
+
+// 			const final = calculatePointC( this.tangent.position, hdg, position );
+
+// 			return new TvPosTheta( final.x, final.y, final.z, hdg );
+
+// 		} else {
+
+// 			return new TvPosTheta( position.x, position.y, position.z, point.hdg );
+
+// 		}
+
+// 	}
+
+// 	getVector3 ( s: number ): Vector3 {
+
+// 		const position = this.tangent.road.getPositionAt( s );
+
+// 		return position.toVector3();
+
+// 	}
+
+// }
+
+// // Function to calculate the position of point C
+// function calculatePointC ( A: Vector3, hdg: number, B: Vector3 ): Vector3 {
+// 	// Step 1: Find the direction vector of A's heading
+// 	const dir_vector_A = new Vector3( Math.cos( hdg ), Math.sin( hdg ), 0 );
+
+// 	// Step 2: Find the vector between A and B
+// 	const AB_vector = B.clone().sub( A );
+
+// 	// Step 3: Project AB_vector onto the direction of dir_vector_A
+// 	const projection_scalar = AB_vector.dot( dir_vector_A );
+// 	const projection_vector = dir_vector_A.clone().multiplyScalar( projection_scalar );
+
+// 	// Step 4: Calculate the position of point C
+// 	const C = A.clone().add( projection_vector );
+
+// 	return C;
+// }
+
+// export class RoadPointerStrategy extends SelectStrategy<RoadControlPoint> {
+
+// 	onPointerDown ( pointerEventData: PointerEventData ): RoadControlPoint {
+
+// 		throw new Error( 'Method not implemented.' );
+
+// 	}
+
+// 	onPointerMoved ( pointerEventData: PointerEventData ): RoadControlPoint {
+
+// 		throw new Error( 'Method not implemented.' );
+
+// 	}
+
+// 	onPointerUp ( pointerEventData: PointerEventData ): RoadControlPoint {
+
+// 		throw new Error( 'Method not implemented.' );
+
+// 	}
+
+// 	dispose (): void {
+// 		throw new Error( 'Method not implemented.' );
+// 	}
+
+// }
+
