8a986dad431a76bb273ada258ec7f62726f6d7b9
Palhimanshu1991 <pal.himanshu1991@gmail.com>
2024-01-19 00:26:12 +0530

    env in project

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+	production: false,
+	api_url: 'https://www.truevision.ai/api/v1',
+	web_url: 'https://localhost:4200',
+	sentry_dsn: null,
+	osc_enabled: true,
+	mixpanel_id: null
+}
