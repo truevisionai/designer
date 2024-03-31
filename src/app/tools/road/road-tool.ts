24f7b36a9110e2f0f739016aaf7fbf6f3c9ca4b0
Himanshu Pal <pal.himanshu1991@gmail.com>
2024-03-31 10:37:22 +0530

    bug fix for removing control point

--- src/app/tools/road/road-tool.ts
+++ src/app/tools/road/road-tool.ts
@@ -485,6 +485,8 @@ export class RoadTool extends BaseTool<AbstractSpline> {
 
 		if ( point instanceof SplineControlPoint ) {
 
+			point.parent.remove( point );
+
 			this.tool.removeControlPoint( point.spline, point );
 
 			this.onSplineUpdated( point.spline );
@@ -492,6 +494,8 @@ export class RoadTool extends BaseTool<AbstractSpline> {
 			AppInspector.clear();
 
 		} else if ( point instanceof RoadControlPoint ) {
+
+			point.parent.remove( point );
 
 			this.tool.removeControlPoint( point.road.spline, point );
 
