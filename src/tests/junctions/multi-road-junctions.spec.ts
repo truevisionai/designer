db719b9484cb51c4f01f5b4830abb79f6264b352
Himanshu Pal <pal.himanshu1991@gmail.com>
2024-06-15 13:54:46 +0530

    test case fix

--- src/tests/junctions/multi-road-junctions.spec.ts
+++ src/tests/junctions/multi-road-junctions.spec.ts
@@ -4,31 +4,19 @@ import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { BaseTest } from "../base-test.spec";
 import { MapService } from "../../app/services/map/map.service";
 import { RoadService } from "../../app/services/road/road.service";
-import { IntersectionService } from "../../app/services/junction/intersection.service";
-import { JunctionService } from "../../app/services/junction/junction.service";
 import { EventServiceProvider } from "../../app/listeners/event-service-provider";
-import { SplineEventListener } from "../../app/listeners/spline-event-listener";
 import { RoadToolService } from "../../app/tools/road/road-tool.service";
-import { SplineManager } from "../../app/managers/spline-manager";
-import { MapValidatorService } from "../../app/services/map/map-validator.service";
-import { IntersectionManager } from "../../app/managers/intersection-manager";
 import { Vector2 } from "three";
-
-const DEFAULT_ROAD_WIDTH = 12.2;
+import { JunctionManager } from "../../app/managers/junction-manager";
 
 describe( 'Multi-RoadJunctionTests', () => {
 
 	let baseTest = new BaseTest();
 	let mapService: MapService;
 	let roadService: RoadService;
-	let intersectionService: IntersectionService;
-	let junctionService: JunctionService;
 	let eventServiceProvider: EventServiceProvider;
-	let splineEventListener: SplineEventListener;
 	let roadToolService: RoadToolService;
-	let splineManager: SplineManager;
-	let mapValidator: MapValidatorService;
-	let intersectionManager: IntersectionManager;
+	let junctionManager: JunctionManager;
 
 	beforeEach( () => {
 
@@ -39,17 +27,12 @@ describe( 'Multi-RoadJunctionTests', () => {
 
 		roadToolService = TestBed.inject( RoadToolService );
 		roadService = roadToolService.roadService;
-		splineManager = TestBed.inject( SplineManager );
-		intersectionManager = TestBed.inject( IntersectionManager );
 
 		mapService = TestBed.inject( MapService );
-		intersectionService = TestBed.inject( IntersectionService );
-		junctionService = TestBed.inject( JunctionService );
-		splineEventListener = TestBed.inject( SplineEventListener );
 		eventServiceProvider = TestBed.inject( EventServiceProvider );
 		eventServiceProvider.init();
 
-		mapValidator = TestBed.inject( MapValidatorService );
+		junctionManager = TestBed.inject( JunctionManager );
 
 	} );
 
@@ -59,10 +42,14 @@ describe( 'Multi-RoadJunctionTests', () => {
 		const vertical = baseTest.createDefaultRoad( roadService, [ new Vector2( 0, -50 ), new Vector2( 0, 50 ) ] )
 		const diagonal = baseTest.createDefaultRoad( roadService, [ new Vector2( -50, -50 ), new Vector2( 50, 50 ) ] )
 
-		intersectionManager.updateIntersections( diagonal.spline );
+		junctionManager.updateJunctions( horiztonal.spline );
 
 		expect( mapService.junctions.length ).toBe( 1 );
 
+		const junction = mapService.getJunction( 1 );
+
+		expect( junction ).toBeDefined();
+		expect( junction.connections.size ).toBe( 30 );
 
 	} );
 
