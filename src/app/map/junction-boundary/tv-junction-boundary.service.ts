a5239b76abc3cd2eb293971d6f3d73e567544c12
Himanshu Pal <pal.himanshu1991@gmail.com>
2025-01-07 18:36:05 +0530

    refactor: remove unused boundary services and streamline TvJunctionBoundaryService

--- src/app/map/junction-boundary/tv-junction-boundary.service.ts
+++ src/app/map/junction-boundary/tv-junction-boundary.service.ts
@@ -3,10 +3,7 @@
  */
 
 import { Injectable } from "@angular/core";
-import { TvJunction } from "../models/junctions/tv-junction";
-import { TvJunctionInnerBoundaryService } from "./tv-junction-inner-boundary.service";
-import { TvBoundarySegmentType, TvJunctionBoundary } from "./tv-junction-boundary";
-import { TvJunctionOuterBoundaryService } from "./tv-junction-outer-boundary.service";
+import { TvJunctionBoundary } from "./tv-junction-boundary";
 import { DebugDrawService } from "app/services/debug/debug-draw.service";
 import { COLOR } from "../../views/shared/utils/colors.service";
 import { Color } from "three";
@@ -16,40 +13,12 @@ import { Color } from "three";
 } )
 export class TvJunctionBoundaryService {
 
-	public static instance: TvJunctionBoundaryService;
-
 	constructor (
-		private outerBoundaryService: TvJunctionOuterBoundaryService,
-		private innerBoundaryService: TvJunctionInnerBoundaryService,
 		private debugService: DebugDrawService,
 	) {
-		TvJunctionBoundaryService.instance = this;
 	}
 
-	update ( junction: TvJunction ): void {
-
-		if ( !junction.innerBoundary ) {
-			junction.innerBoundary = new TvJunctionBoundary();
-		} else {
-			junction.innerBoundary.clearSegments();
-		}
-
-		this.innerBoundaryService.update( junction, junction.innerBoundary );
-
-		if ( !junction.outerBoundary ) {
-			junction.outerBoundary = new TvJunctionBoundary();
-		} else {
-			junction.outerBoundary.clearSegments();
-		}
-
-		this.outerBoundaryService.update( junction, junction.outerBoundary );
-
-		// this.debugBoundary( junction.innerBoundary );
-
-	}
-
-	// eslint-disable-next-line no-undef
-	debugBoundary ( boundary: TvJunctionBoundary, color: number = COLOR.RED ): void {
+	private debugBoundary ( boundary: TvJunctionBoundary, color: number = COLOR.RED ): void {
 
 		boundary.getSegments().forEach( segment => {
 
