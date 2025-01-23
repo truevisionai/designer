6fb9887e5b2a09bb3b0932e52f9d872366a6709a
Himanshu Pal <pal.himanshu1991@gmail.com>
2025-01-23 12:45:16 +0530

    2 classes for temp

--- src/app/core/visualizers/spline-visualizer.ts
+++ src/app/core/visualizers/spline-visualizer.ts
@@ -6,21 +6,19 @@ import { Injectable } from "@angular/core";
 import { BaseVisualizer } from "./base-visualizer";
 import { AbstractSpline } from "../shapes/abstract-spline";
 import { SplineDebugService } from "../../services/debug/spline-debug.service";
-import { SplineView } from "app/tools/lane/visualizers/SplineView";
-import { IViewModel } from "app/tools/lane/visualizers/IViewModel";
-import { IView } from "app/tools/lane/visualizers/IView";
-// import { COLOR } from "../../views/shared/utils/colors.service";
+import { COLOR } from "../../views/shared/utils/colors.service";
 import { SceneService } from "app/services/scene.service";
-// import { IView, SplineBorderView, SplineView } from "app/tools/lane/visualizers/lane-tool-road-visualizer";
-// import { Object3D } from "three";
+import { IView } from "app/tools/lane/visualizers/IView";
+import { SplineView } from "app/tools/lane/visualizers/SplineView";
 
 @Injectable( {
 	providedIn: 'root'
 } )
-export abstract class SplineVisualizer<T extends AbstractSpline> extends BaseVisualizer<T> {
+export abstract class NewSplineVisualizerWithView<T extends AbstractSpline> extends BaseVisualizer<T> {
 
 	private views = new Map<AbstractSpline, IView>();
 
+	// NOTE: this visualizer is not used in the codebase
 	constructor (
 		private splineDebugService: SplineDebugService,
 	) {
@@ -37,31 +35,11 @@ export abstract class SplineVisualizer<T extends AbstractSpline> extends BaseVis
 
 	onUpdated ( spline: AbstractSpline ): void {
 
-		// this.splineDebugService.removeBorder( spline );
-		// this.splineDebugService.removeControlPoints( spline );
-		// this.splineDebugService.removePolyline( spline );
-		// this.splineDebugService.removeReferenceLine( spline );
-		// this.splineDebugService.removeCurvature( spline );
-
-		// this.splineDebugService.showBorder( spline );
-		// this.splineDebugService.showControlPoints( spline );
-		// this.splineDebugService.showPolyline( spline );
-		// this.splineDebugService.showReferenceLine( spline );
-		// this.splineDebugService.showCurvature( spline );
-
 		this.views.get( spline )?.update();
 
 	}
 
-	onClearHighlight (): void {
-
-		// this.highlighted.forEach( road => {
-
-		// 	this.onRemoved( road );
-
-		// } )
-
-	}
+	onClearHighlight (): void { }
 
 	onHighlight ( object: AbstractSpline ): void {
 
@@ -81,23 +59,11 @@ export abstract class SplineVisualizer<T extends AbstractSpline> extends BaseVis
 
 	onDefault ( object: AbstractSpline ): void {
 
-		// this.splineDebugService.removeBorder( object );
-		// this.splineDebugService.removeControlPoints( object );
-		// this.splineDebugService.removePolyline( object );
-		// this.splineDebugService.removeReferenceLine( object );
-		// this.splineDebugService.removeCurvature( object );
-
 		this.views.get( object ).onMouseOut();
 
 	}
 
 	onSelected ( object: AbstractSpline ): void {
-
-		// this.splineDebugService.showBorder( object, 4, COLOR.RED );
-		// this.splineDebugService.showControlPoints( object );
-		// this.splineDebugService.showPolyline( object );
-		// this.splineDebugService.showReferenceLine( object );
-		// this.splineDebugService.showCurvature( object );
 
 		this.views.get( object )?.onClick();
 
@@ -117,7 +83,106 @@ export abstract class SplineVisualizer<T extends AbstractSpline> extends BaseVis
 
 	clear (): void {
 
-		// this.splineDebugService.clear();
+		this.highlighted.clear();
+
+	}
+
+}
+
+@Injectable( {
+	providedIn: 'root'
+} )
+export abstract class SplineVisualizer<T extends AbstractSpline> extends BaseVisualizer<T> {
+
+	constructor (
+		private splineDebugService: SplineDebugService,
+	) {
+
+		super();
+
+	}
+
+	onAdded ( object: AbstractSpline ): void {
+
+		this.splineDebugService.showBorder( object );
+
+	}
+
+	onUpdated ( spline: AbstractSpline ): void {
+
+		this.splineDebugService.removeBorder( spline );
+		this.splineDebugService.removeControlPoints( spline );
+		this.splineDebugService.removePolyline( spline );
+		this.splineDebugService.removeReferenceLine( spline );
+		this.splineDebugService.removeCurvature( spline );
+
+		this.splineDebugService.showBorder( spline );
+		this.splineDebugService.showControlPoints( spline );
+		this.splineDebugService.showPolyline( spline );
+		this.splineDebugService.showReferenceLine( spline );
+		this.splineDebugService.showCurvature( spline );
+
+	}
+
+	onClearHighlight (): void {
+
+		this.highlighted.forEach( road => {
+
+			this.onRemoved( road );
+
+		} )
+
+	}
+
+	onHighlight ( object: AbstractSpline ): void {
+
+		this.splineDebugService.showBorder( object );
+
+	}
+
+	onDefault ( object: AbstractSpline ): void {
+
+		this.splineDebugService.removeBorder( object );
+		this.splineDebugService.removeControlPoints( object );
+		this.splineDebugService.removePolyline( object );
+		this.splineDebugService.removeReferenceLine( object );
+		this.splineDebugService.removeCurvature( object );
+
+	}
+
+	onSelected ( object: AbstractSpline ): void {
+
+		this.splineDebugService.showBorder( object, 4, COLOR.RED );
+		this.splineDebugService.showControlPoints( object );
+		this.splineDebugService.showPolyline( object );
+		this.splineDebugService.showReferenceLine( object );
+		this.splineDebugService.showCurvature( object );
+
+	}
+
+	onUnselected ( object: AbstractSpline ): void {
+
+		this.splineDebugService.removeBorder( object );
+		this.splineDebugService.removeControlPoints( object );
+		this.splineDebugService.removePolyline( object );
+		this.splineDebugService.removeReferenceLine( object );
+		this.splineDebugService.removeCurvature( object );
+
+	}
+
+	onRemoved ( object: AbstractSpline ): void {
+
+		this.splineDebugService.removeBorder( object );
+		this.splineDebugService.removeControlPoints( object );
+		this.splineDebugService.removePolyline( object );
+		this.splineDebugService.removeReferenceLine( object );
+		this.splineDebugService.removeCurvature( object );
+
+	}
+
+	clear (): void {
+
+		this.splineDebugService.clear();
 		this.highlighted.clear();
 
 	}
